af90b6e19fdd16dbad4c0cb33182e58b4d40ae7f
Juan Román <juanromanch@gmail.com>
2019-08-14 04:04:18 +0200

    Configurado acceso a base de datos y FOSUserBundle

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+// src/Controller/AdminController.php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController as BaseAdminController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends BaseAdminController
+{
+    public function createNewRepresenEntity()
+    {
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function persistRepresenEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::persistEntity($user);
+    }
+    public function updateRepresenEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::updateEntity($user);
+    }
+}
